16e829bbfdcd2a3b9586e76c2b9ce86dc892ca07
Scott Miller <scott@millergeek.xyz>
2019-03-11 09:46:39 -0400

    init

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+# Start or re-use a gpg-agent.
+#
+gpgconf --launch gpg-agent
+
+# Ensure that GPG Agent is used as the SSH agent
+set -e SSH_AUTH_SOCK
+set -U -x SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+ln -sf $(pwd)/fish/fishfile ~/.config/fish/fishfile
+ln -sf $(pwd)/fish/config.fish ~/.config/fish/config.fish
