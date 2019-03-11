b50a8b737dc4dc523625c8eefbe6d6c19b8bd89b
Scott Miller <scott@millergeek.xyz>
2019-03-11 12:45:13 -0400

    update all the things

--- fish/config.fish
+++ fish/config.fish
@@ -15,3 +15,8 @@ gpgconf --launch gpg-agent
 # Ensure that GPG Agent is used as the SSH agent
 set -e SSH_AUTH_SOCK
 set -U -x SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
+
+# Enable npx auto-fallback
+source (npx --shell-auto-fallback fish | psub)
+
+thefuck --alias | source
--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install python2 python3 nano gnupg pinentry-mac node
 ln -sf $(pwd)/fish/fishfile ~/.config/fish/fishfile
 ln -sf $(pwd)/fish/config.fish ~/.config/fish/config.fish
+mkdir ~/.gnupg
+ln -sf $(pwd)/gpg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+# Start or re-use a gpg-agent.
+#
+gpgconf --launch gpg-agent
+
+# Ensure that GPG Agent is used as the SSH agent
+set -e SSH_AUTH_SOCK
+set -U -x SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
+
+git clone git@github.com:scopatz/nanorc.git ~/.nano
+ln -sf ~/.nano/nanorc ~/.nanorc
