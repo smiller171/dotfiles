3d55cc28b1dde5fd8ebee2065857002f7e1a3c76
Scott Miller <scott@millergeek.xyz>
2019-03-21 14:58:51 -0400

    add kubectl completions and tflint

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install python2 python3 nano gnupg pinentry-mac node terraform
+brew tap wata727/tflint
+brew install python2 python3 nano gnupg pinentry-mac node terraform tflint
 pip3 install virtualenv virtualfish
 ln -sf $(pwd)/fish/fishfile ~/.config/fish/fishfile
 ln -sf $(pwd)/fish/config.fish ~/.config/fish/config.fish
+mkdir -p ~/.config/fish/completions
+curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish > ~/.config/fish/completions/docker.fish
 mkdir ~/.gnupg
 ln -sf $(pwd)/gpg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
 # Start or re-use a gpg-agent.
