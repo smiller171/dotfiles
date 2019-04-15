61f5f8aa55b33e68f031dd754b30e6b74fdcaa82
Ubuntu <ubuntu@ip-172-31-64-240.ec2.internal>
2019-04-15 16:41:47 +0000

    update setup

--- setup.sh
+++ setup.sh
@@ -1,25 +1,33 @@
 #!/usr/bin/env bash
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap wata727/tflint
-brew install python2 python3 nano gnupg pinentry-mac node terraform tflint mkcert jq exercism
-pip3 install virtualenv virtualfish
+set -ex
+case $(uname) in
+  'Darwin')
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew tap wata727/tflint
+    brew install python2 python3 nano gnupg pinentry-mac node terraform tflint mkcert jq exercism fish
+    mkcert -install
+    ;;
+  'Linux')
+    apt-add-repository ppa:fish-shell/release-3
+    apt-get update
+    apt-get install -y build-essential python python3 python-dev python3-dev python-pip python3-pip gnupg fish
+    snap install jq
+    # curl -sSL https://get.docker.com | /bin/bash
+    # curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    # chmod +x /usr/local/bin/docker-compose
+    ;;
+esac
+
+pip3 install virtualenv virtualfish thefuck awscli
+pip install virtualenv virtualfish
 ln -sf $(pwd)/fish/fishfile ~/.config/fish/fishfile
 ln -sf $(pwd)/fish/config.fish ~/.config/fish/config.fish
 mkdir -p ~/.config/fish/completions
 curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish > ~/.config/fish/completions/docker.fish
 curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/fish/docker-compose.fish > ~/.config/fish/completions/docker-compose.fish
-mkdir ~/.gnupg
+mkdir -p ~/.gnupg
 ln -sf $(pwd)/gpg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
-# Start or re-use a gpg-agent.
-#
-gpgconf --launch gpg-agent
-
-# Ensure that GPG Agent is used as the SSH agent
-set -e SSH_AUTH_SOCK
-set -U -x SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
+chown -R $(logname):$(logname) ~/.config
 
 git clone git@github.com:scopatz/nanorc.git ~/.nano
 ln -sf ~/.nano/nanorc ~/.nanorc
-
-# CA setup
-mkcert -install
