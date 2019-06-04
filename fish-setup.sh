1deb40e8bfd9d73f44de45ead2476052d961d009
Scott Miller <scott@millergeek.xyz>
2019-06-04 14:04:31 -0400

    create config dir if needed

--- fish-setup.sh
+++ fish-setup.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-apt-add-repository ppa:fish-shell/release-3
-apt-get update
-apt-get install -y fish
+mkdir -p ~/.config/fish
 ln -sf "$(pwd)/fish/fishfile" ~/.config/fish/fishfile
 ln -sf "$(pwd)/fish/config.fish" ~/.config/fish/config.fish
 chown -R "$(logname)":"$(logname)" ~/.config
