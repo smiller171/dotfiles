e1cb17b2f117171f21a99e44e892c6b83bab50fd
Scott Miller <scott@millergeek.xyz>
2019-06-04 14:10:07 -0400

    login user fallback

--- fish-setup.sh
+++ fish-setup.sh
@@ -2,4 +2,5 @@
 mkdir -p ~/.config/fish
 ln -sf "$(pwd)/fish/fishfile" ~/.config/fish/fishfile
 ln -sf "$(pwd)/fish/config.fish" ~/.config/fish/config.fish
-chown -R "$(logname)":"$(logname)" ~/.config
+login_user="$(logname)"
+chown -R "${login_user:=root}":"${login_user:=root}" ~/.config
