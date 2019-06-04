4d2acf9e00295957f62f8a571e854d894c6d1f60
Scott Miller <scott@millergeek.xyz>
2019-06-04 14:16:01 -0400

    login user fallback

--- setup.sh
+++ setup.sh
@@ -11,7 +11,6 @@ case $(uname) in
     apt-add-repository ppa:fish-shell/release-3
     apt-get update
     apt-get install -y build-essential python python3 python-dev python3-dev python-pip python3-pip gnupg fish
-    snap install jq
     # curl -sSL https://get.docker.com | /bin/bash
     # curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     # chmod +x /usr/local/bin/docker-compose
@@ -20,6 +19,7 @@ esac
 
 pip3 install virtualenv virtualfish thefuck awscli
 pip install virtualenv virtualfish
+mkdir -p ~/.config/fish
 ln -sf "$(pwd)/fish/fishfile" ~/.config/fish/fishfile
 ln -sf "$(pwd)/fish/config.fish" ~/.config/fish/config.fish
 mkdir -p ~/.config/fish/completions
@@ -27,7 +27,8 @@ curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/f
 curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/fish/docker-compose.fish > ~/.config/fish/completions/docker-compose.fish
 mkdir -p ~/.gnupg
 ln -sf "$(pwd)/gpg/gpg-agent.conf" ~/.gnupg/gpg-agent.conf
-chown -R "$(logname)":"$(logname)" ~/.config
+login_user="$(logname)"
+chown -R "${login_user:=root}":"${login_user:=root}" ~/.config
 
 git clone git@github.com:scopatz/nanorc.git ~/.nano
 ln -sf ~/.nano/nanorc ~/.nanorc
