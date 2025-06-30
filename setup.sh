609bcf44638ca38ce00b14d1ecb7f7132d6c9fae
Scott Miller <scott.miller171@gmail.com>
2025-06-30 14:33:14 -0400

    add Bitwarden

--- setup.sh
+++ setup.sh
@@ -18,207 +18,105 @@ ln -sf "$(pwd)/fish/config.fish" ~/.config/fish/config.fish
 ln -sf "$(pwd)/zed" ~/.config/zed
 ln -sf "$(pwd)/awsconfig" ~/.aws/config
 mkdir -p ~/.config/fish/completions
-curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish > ~/.config/fish/completions/docker.fish
-curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/fish/docker-compose.fish > ~/.config/fish/completions/docker-compose.fish
+curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish >~/.config/fish/completions/docker.fish
+curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/fish/docker-compose.fish >~/.config/fish/completions/docker-compose.fish
 
 case $(uname) in
-  'Darwin')
-    curl -sL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaCode.zip -o ~/Downloads/CascadiaCode.zip
-    mkdir -p fontfiles
-    cd fontfiles
-    unzip ~/Downloads/CascadiaCode.zip
-    rm ./*Windows* || echo 'No windows-specific font files'
-    mv ./*.ttf ~/Library/Fonts/
-    cd ..
-    rm -rf fontfiles
-    rm ~/Downloads/CascadiaCode.zip
+'Darwin')
+  curl -sL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaCode.zip -o ~/Downloads/CascadiaCode.zip
+  mkdir -p fontfiles
+  cd fontfiles
+  unzip ~/Downloads/CascadiaCode.zip
+  rm ./*Windows* || echo 'No windows-specific font files'
+  mv ./*.ttf ~/Library/Fonts/
+  cd ..
+  rm -rf fontfiles
+  rm ~/Downloads/CascadiaCode.zip
 
-    xcode-select --install || true
+  xcode-select --install || true
+  case $(uname -m) in
+  'x86_64')
+    eval "$(/usr/local/bin/brew shellenv)"
+    ;;
+  'arm64')
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    ;;
+  esac
+
+  if brew --version; then
+    echo "Homebrew is already installed"
+  else
+    echo "Installing Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     case $(uname -m) in
-      'x86_64')
-        eval "$(/usr/local/bin/brew shellenv)"
-        ;;
-      'arm64')
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-        ;;
+    'x86_64')
+      eval "$(/usr/local/bin/brew shellenv)"
+      ;;
+    'arm64')
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+      ;;
     esac
+  fi
 
-    if brew --version; then
-      echo "Homebrew is already installed"
-    else
-      echo "Installing Homebrew"
-      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-      case $(uname -m) in
-        'x86_64')
-          eval "$(/usr/local/bin/brew shellenv)"
-          ;;
-        'arm64')
-          eval "$(/opt/homebrew/bin/brew shellenv)"
-          ;;
-      esac
-    fi
+  brew tap wata727/tflint
+  brew tap yleisradio/terraforms
+  brew tap jakehilborn/jakehilborn
+  brew install \
+    arc argocd autoconf aws-iam-authenticator aws-sso-util aws-vault awscli bash ca-certificates coreutils curl diffutils displayplacer docker docker-credential-helper-ecr fish fluxcd/tap/flux fswatch gawk gcc gd gettext gh git git-filter-repo glab glib gnu-sed gnupg gnutls gping grep hadolint helm iterm2 jq k9s keybase keymapp kreuzwerker/taps/m1-terraform-provider-helper kubectl kubernetes-cli kustomize libffi libfido2 libgcrypt mas mkcert nano node nushell openssh orbstack pinentry-mac pinentry pipx pkg-config postman pre-commit pyenv readline shellcheck shfmt smiller171/tap/kubecolor sops ssh-copy-id sshuttle starship telnet terminal-notifier terraform-docs terrascan tenv tflint tfsec thefuck theseal/ssh-askpass/ssh-askpass trunk-io velero watch wget xclip ykman yt-music yq
+  brew install --cask beeper bettertouchtool keybase obsidian orbstack rocket signal visual-studio-code zed
+  mas install 803453959 || true  # Slack
+  mas install 1352778147 || true # Bitwarden
+  mas install 1497506650 || true # Yubico Authenticator
+  pipx install aws-export-credentials || true
 
-    brew tap wata727/tflint
-    brew tap yleisradio/terraforms
-    brew tap jakehilborn/jakehilborn
-    brew install \
-      arc\
-      argocd\
-      autoconf\
-      aws-iam-authenticator\
-      aws-sso-util\
-      aws-vault\
-      awscli\
-      bash\
-      ca-certificates\
-      coreutils\
-      curl\
-      diffutils\
-      displayplacer\
-      docker\
-      docker-credential-helper-ecr\
-      fish\
-      fluxcd/tap/flux\
-      fswatch\
-      gawk\
-      gcc\
-      gd\
-      gettext\
-      gh\
-      git\
-      git-filter-repo\
-      glab\
-      glib\
-      gnu-sed\
-      gnupg\
-      gnutls\
-      gping\
-      grep\
-      hadolint\
-      helm\
-      iterm2\
-      jq\
-      k9s\
-      keybase\
-      keymapp\
-      kreuzwerker/taps/m1-terraform-provider-helper\
-      kubectl\
-      kubernetes-cli\
-      kustomize\
-      libffi\
-      libfido2\
-      libgcrypt\
-      mas\
-      mkcert\
-      nano\
-      node\
-      nushell\
-      openssh\
-      orbstack\
-      pinentry-mac\
-      pinentry\
-      pipx\
-      pkg-config\
-      postman\
-      pre-commit\
-      pyenv\
-      readline\
-      shellcheck\
-      shfmt\
-      smiller171/tap/kubecolor\
-      sops\
-      ssh-copy-id\
-      sshuttle\
-      starship\
-      telnet\
-      terminal-notifier\
-      terraform-docs\
-      terrascan\
-      tenv\
-      tflint\
-      tfsec\
-      thefuck\
-      theseal/ssh-askpass/ssh-askpass\
-      trunk-io\
-      velero\
-      watch\
-      wget\
-      xclip\
-      ykman\
-      yt-music\
-      yq\
-      yubico-authenticator
-    brew install --cask\
-      beeper\
-      bettertouchtool\
-      keybase\
-      obsidian\
-      orbstack\
-      rocket\
-      signal\
-      visual-studio-code\
-      zed
-    mas install 803453959 || true  # Slack
-    pipx install aws-export-credentials || true
+  # dockutil
+  dockutil_version=$(curl -sSL \
+    -H 'Accept: application/vnd.github+json' \
+    -H 'X-GitHub-Api-Version: 2022-11-28' \
+    https://api.github.com/repos/kcrawford/dockutil/releases/latest | jq -r '.tag_name')
+  curl -sSL "https://github.com/kcrawford/dockutil/releases/download/${dockutil_version}/dockutil-${dockutil_version}.pkg" >/tmp/dockutil.pkg
+  sudo installer -pkg /tmp/dockutil.pkg -target /
+  ./dock-cleanup.fish || true
 
-    # dockutil
-    dockutil_version=$(curl -sSL \
-             -H 'Accept: application/vnd.github+json' \
-             -H 'X-GitHub-Api-Version: 2022-11-28' \
-             https://api.github.com/repos/kcrawford/dockutil/releases/latest | jq -r '.tag_name')
-    curl -sSL "https://github.com/kcrawford/dockutil/releases/download/${dockutil_version}/dockutil-${dockutil_version}.pkg" > /tmp/dockutil.pkg
-    sudo installer -pkg /tmp/dockutil.pkg -target /
-    ./dock-cleanup.fish || true
+  ln -sf "$(pwd)/iterm2" ~/.config/iterm2
+  defaults write com.googlecode.iterm2 PrefsCustomFolder -string "/Users/${login_user}/.config/iterm2"
+  defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+  launchctl disable user/${UID}/com.openssh.ssh-agent
 
-    ln -sf "$(pwd)/iterm2" ~/.config/iterm2
-    defaults write com.googlecode.iterm2 PrefsCustomFolder -string "/Users/${login_user}/.config/iterm2"
-    defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
-    launchctl disable user/${UID}/com.openssh.ssh-agent
+  mkcert -install || true
+  chown -R "${login_user:=root}":staff ~/.config
+  # One-time Bash profile setup for VSCode
+  if [ ! -f ~/.bashSetup ]; then
+    touch ~/.bash_profile
+    # shellcheck disable=SC2016
+    case $(uname -m) in
+    'x86_64')
+      echo 'eval "$(/usr/local/bin/brew shellenv)"' >>~/.bash_profile
+      ;;
+    'arm64')
+      echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>~/.bash_profile
+      ;;
+    esac
+    # shellcheck disable=SC2016
+    echo 'export PATH=~/.bin:~/.local/bin:$(brew --prefix)/bin:$(brew --prefix)/opt/coreutils/libexec/gnubin:$(brew --prefix)/opt/grep/libexec/gnubin:${PATH}' >>~/.bash_profile
+  fi
+  touch ~/.bashSetup
 
-    mkcert -install || true
-    chown -R "${login_user:=root}":staff ~/.config
-    # One-time Bash profile setup for VSCode
-    if [ ! -f ~/.bashSetup ]
-    then
-      touch ~/.bash_profile
-      # shellcheck disable=SC2016
-      case $(uname -m) in
-        'x86_64')
-          echo 'eval "$(/usr/local/bin/brew shellenv)"' >> ~/.bash_profile
-          ;;
-        'arm64')
-          echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bash_profile
-          ;;
-      esac
-      # shellcheck disable=SC2016
-      echo 'export PATH=~/.bin:~/.local/bin:$(brew --prefix)/bin:$(brew --prefix)/opt/coreutils/libexec/gnubin:$(brew --prefix)/opt/grep/libexec/gnubin:${PATH}' >> ~/.bash_profile
-    fi
-    touch ~/.bashSetup
+  ;;
+'Linux')
+  sudo ln -sf /usr/bin/fish /usr/local/bin/fish || ln -sf /usr/bin/fish /usr/local/bin/fish
+  sudo apt-get update || apt-get update
+  PACKAGES=(
+    software-properties-common build-essential gnupg scdaemon python3 python3-dev python3-pip fish jq watch
+  )
+  sudo apt-get install -y "${PACKAGES[@]}" || apt-get install -y "${PACKAGES[@]}"
 
-    ;;
-  'Linux')
-    sudo ln -sf /usr/bin/fish /usr/local/bin/fish || ln -sf /usr/bin/fish /usr/local/bin/fish
-    sudo apt-get update || apt-get update
-    PACKAGES=(
-      software-properties-common\
-      build-essential\
-      gnupg\
-      scdaemon\
-      python3\
-      python3-dev\
-      python3-pip\
-      fish\
-      jq\
-      watch
-    )
-    sudo apt-get install -y "${PACKAGES[@]}" || apt-get install -y "${PACKAGES[@]}"
+  curl -sS https://starship.rs/install.sh | sh -s -- --yes
 
-    curl -sS https://starship.rs/install.sh | sh -s -- --yes
-
-    # curl -sSL https://get.docker.com | /bin/bash
-    # curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    # chmod +x /usr/local/bin/docker-compose
-    ;;
+  # curl -sSL https://get.docker.com | /bin/bash
+  # curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  # chmod +x /usr/local/bin/docker-compose
+  ;;
 esac
 
 export PYENV_ROOT="$HOME/.pyenv"
@@ -235,7 +133,6 @@ sudo chsh -s "$(which fish)"
 git clone https://github.com/scopatz/nanorc.git ~/.nano || true
 ln -sf ~/.nano/nanorc ~/.nanorc
 
-
 mkdir -p ~/.git_template
 ln -sf "$(pwd)/git/.gitconfig" ~/.gitconfig
 ln -sf "$(pwd)/git/.gitconfig-apothesource" ~/.gitconfig-apothesource
@@ -245,14 +142,13 @@ ln -sf "$(pwd)/prompt/starship.toml" ~/.config/starship.toml
 
 ln -sf "$(pwd)/homedir/.editorconfig" ~/.editorconfig
 
-
 company=$(curl -Ls \
   -H 'Accept: application/vnd.github+json' \
   -H 'X-GitHub-Api-Version: 2022-11-28' \
   https://api.github.com/users/smiller171 | jq -r '.company')
 
 case "${company}" in
-  'Apothesource')
-    source apothesource.sh
-    ;;
+'Apothesource')
+  source apothesource.sh
+  ;;
 esac
